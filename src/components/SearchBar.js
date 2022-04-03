d678d1f152cd260b31ffb7a52c4420493f8c2b49
dhruv50ae <linkdp2012@gmail.com>
2022-04-03 18:20:43 +0100

    Rendered input field with state

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,9 +1,24 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class SearchBar extends Component {
+  state = { term: "" };
+  onInputChange = (e) => {
+    this.setState({ term: e.target.value });
+  };
   render() {
     return (
-      <div>SearchBar</div>
-    )
+      <div className="search-bar ui segment">
+        <form className="ui form">
+          <div className="field">
+            <label>Video Search</label>
+            <input
+              value={this.state.term}
+              onChange={this.onInputChange}
+              type="text"
+            />
+          </div>
+        </form>
+      </div>
+    );
   }
 }
