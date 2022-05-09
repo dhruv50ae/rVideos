f1b4cfafa448d4f3f6a85b133d151b16f504000c
dhruv50ae <linkdp2012@gmail.com>
2022-05-09 13:00:01 +0100

    Passed down onVideoSelect

--- src/components/App.js
+++ src/components/App.js
@@ -5,17 +5,23 @@ import youtube from "../apis/youtube";
 import VideoList from "./VideoList";
 
 export default class App extends Component {
-  state = { videos: [] };
+  state = { videos: [], selectedVideo: null };
   onTermSubmit = async (term) => {
     console.log(term);
     const response = await youtube.get("/search", { params: { q: term } });
     this.setState({ videos: response.data.items });
   };
+  onVideoSelect = (video) => {
+    console.log("from the app", video);
+  };
   render() {
     return (
       <div className="ui container">
         <SearchBar onFormSubmit={this.onTermSubmit} />
-        <VideoList videos={this.state.videos} />
+        <VideoList
+          onVideoSelect={this.onVideoSelect}
+          videos={this.state.videos}
+        />
       </div>
     );
   }
