a2a9f21efb8a30b81819a73b878852bd60955299
dhruv50ae <linkdp2012@gmail.com>
2022-05-09 13:06:34 +0100

    Created a functional detail component

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import SearchBar from "./SearchBar";
 import React, { Component } from "react";
 import youtube from "../apis/youtube";
 import VideoList from "./VideoList";
+import VideoDetail from "./VideoDetail";
 
 export default class App extends Component {
   state = { videos: [], selectedVideo: null };
@@ -12,12 +13,13 @@ export default class App extends Component {
     this.setState({ videos: response.data.items });
   };
   onVideoSelect = (video) => {
-    console.log("from the app", video);
+    this.setState({ selectedVideo: video });
   };
   render() {
     return (
       <div className="ui container">
         <SearchBar onFormSubmit={this.onTermSubmit} />
+        <VideoDetail video={this.state.selectedVideo} />
         <VideoList
           onVideoSelect={this.onVideoSelect}
           videos={this.state.videos}
